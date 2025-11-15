4964621f8711522eba08d9f2a4d9ab8141801e1d
Nirupam Das <nirupam.astro@gmail.com>
2025-11-15 21:50:48 +0530

    first commit

---
+++ src/main/java/io/github/nirupam/sentiment_analysis_service/SentimentAnalysisServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.nirupam.sentiment_analysis_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentimentAnalysisServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SentimentAnalysisServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/nirupam/sentiment_analysis_service/SentimentAnalysisServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nirupam.sentiment_analysis_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SentimentAnalysisServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
